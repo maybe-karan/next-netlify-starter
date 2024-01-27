4a3d3eb4031593b4275730645a1a968a4f15df6c
Karan Giri <91467994+maybe-karan@users.noreply.github.com>
2024-01-28 03:11:28 +0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,27 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+import React, { useState, useEffect } from 'react';
+import moment from 'moment-timezone';
 
-export default function Home() {
+const DualTimeClock = () => {
+  const [localTime, setLocalTime] = useState(moment());
+  const [hkTime, setHKTime] = useState(moment().tz('Asia/Hong_Kong'));
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setLocalTime(moment());
+      setHKTime(moment().tz('Asia/Hong_Kong'));
+    }, 1000);
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
+
   return (
-    <div className="container">
-      <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-      </main>
-
-      <Footer />
+    <div>
+      <h2>Local Time: {localTime.format('h:mm:ss A')}</h2>
+      <h2>Hong Kong Time: {hkTime.format('h:mm:ss A')}</h2>
     </div>
-  )
-}
+  );
+};
+
+export default DualTimeClock;
